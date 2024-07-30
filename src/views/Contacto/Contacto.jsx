f34926584345578dcbd343b72e9223fcb3ac82db
Nayrb29 <bryan201429@gmail.com>
2024-07-30 14:41:29 -0500

    contacto2

--- src/views/Contacto/Contacto.jsx
+++ src/views/Contacto/Contacto.jsx
@@ -15,8 +15,13 @@ export default function Contacto(){
                 
             </section>
             <section id='section2'>
-
-
+                <div className='leftDiv'>
+                    
+                </div>
+                <div className='rightDiv'>
+                    <h2>MAPA: </h2>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36270.689610188514!2d-71.425016445661!3d-16.294937959858316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424fcc9f1394f9%3A0x319d57bcb502bf3c!2sMisti!5e1!3m2!1ses!2spe!4v1722367496961!5m2!1ses!2spe" width="80%" height="80%" style={{border:0,borderRadius:'10px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </section>
             <section id='section3'>
 
