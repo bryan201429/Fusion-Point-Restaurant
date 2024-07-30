e47896154898a7dc6ec376ca6c3d456be472ac66
Nayrb29 <bryan201429@gmail.com>
2024-07-30 13:40:19 -0500

    Contact view 1

--- src/views/Contacto/Contacto.jsx
+++ src/views/Contacto/Contacto.jsx
@@ -1,8 +1,27 @@
+import './Contacto.css'
+import rest from '../../assets/backgrounds/contactBack.jpg'
+import arrow from '../../assets/icons/downArrow.svg'
 export default function Contacto(){
 
     return(
-        <div>
-            Contacto
+        <div id='ContactContainer'>
+            <section id='section1'>
+                <img id='imgb1' src={rest}></img>
+                <h1>SIGUE EL CAMINO AL SABOR SOÑADO</h1>
+                <div id='conoceBtn'>
+                    <h2>Conoce más</h2>
+                    <img src={arrow}></img>
+                </div>
+                
+            </section>
+            <section id='section2'>
+
+
+            </section>
+            <section id='section3'>
+
+                
+            </section>
         </div>
     )
 }
